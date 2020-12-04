efe90a24cba3e62d48a9e664b3612d7045e3ab6f
peres <filipmeina@gmail.com>
2020-12-04 18:31:26 +0100

    added report and stats htmls, updated core views

--- application/core/views.py
+++ application/core/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, Blueprint
+from flask_login import login_required, current_user
 
 core = Blueprint('core', __name__)
 
@@ -6,3 +7,9 @@ core = Blueprint('core', __name__)
 @core.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+
+@login_required
+@core.route('/stats')
+def stats():
+    return render_template('stats.html')
